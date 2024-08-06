53552c0a49a315252383dad47b1c2b261887ed88
niicez <tuandaris1@gmail.com>
2024-08-07 01:42:07 +0700

    refactor(): rename function & var

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use trainer_base::process::{get_pid_by_name, get_process_base, get_process_info}
 extern crate litcrypt;
 litcrypt::use_litcrypt!();
 
-fn get_address_by_offsets(
+fn get_pointer_address(
     base_address: usize,
     offsets: Vec<usize>,
     process: &Process,
@@ -21,7 +21,7 @@ fn get_address_by_offsets(
                 Error::new(
                     std::io::ErrorKind::Other,
                     format!(
-                        "[get_address_by_offsets]: Failed to read memory: {} {}",
+                        "[get_pointer_address]: Failed to read memory: {} {}",
                         e, offset
                     ),
                 )
@@ -30,7 +30,7 @@ fn get_address_by_offsets(
         result_address = u32::from_le_bytes(buffer) as usize;
 
         println!(
-            "[get_address_by_offsets]: get - {} -> {:#x}",
+            "[get_pointer_address]: get - {} -> {:#x}",
             index, result_address
         );
     }
@@ -42,6 +42,7 @@ fn main() {
     let b_process_name = litcrypt::lc!("ac_client.exe");
     let process_name: &str = b_process_name.as_str();
 
+    let localplayer_ptr_offset: usize = 0x0018B0B8;
     let localplayer_offset: usize = 0x404;
     let health_offset: usize = 0x04;
 
@@ -83,17 +84,20 @@ fn main() {
         process_name, base_address as usize
     );
 
-    let localplayer_pointer_address: usize =
-        match get_address_by_offsets(base_address as usize, vec![0x0018B0B8, 0x0], &process) {
-            Ok(address) => address,
-            Err(e) => {
-                println!("{}", e);
-                pause();
-                return;
-            }
-        };
+    let localplayer_ptr_address: usize = match get_pointer_address(
+        base_address as usize,
+        vec![localplayer_ptr_offset, 0x0],
+        &process,
+    ) {
+        Ok(address) => address,
+        Err(e) => {
+            println!("{}", e);
+            pause();
+            return;
+        }
+    };
 
-    let localplayer_address = localplayer_pointer_address + localplayer_offset;
+    let localplayer_address = localplayer_ptr_address + localplayer_offset;
     println!("LocalPlayer Address {:#x}", localplayer_address as usize);
 
     let health_value: usize = localplayer_address + health_offset;
